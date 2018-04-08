cc64d682e3ffc254b89f5113495c607ac674e31e
Sachin0s <rnd.sachin@gmail.com>
2018-04-08 11:34:36 +0530

    js file upload

---
+++ JSFile.js
@@ -0,0 +1,30 @@
+var user = { name: "John", age: 34, hobby: "Soccor", IsMarried: false;}
+user.FavFood = "Spinach";
+
+var todos = [
+    "Clean Room",
+    "Brush Teeths",
+    "Excercise",
+    "Study",
+    "eat healthy"
+]
+
+var todosLength = todos.length;
+for (int i = 0; i < todosLength;i++)
+{
+
+}
+
+todos.forEach(function (todo, i) {
+    console.log(todo,i)
+})
+
+function thisIsFunction() {
+
+}
+
+var obj = {
+    thisIsMethod: function() {
+
+    }
+}
